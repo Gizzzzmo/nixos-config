71937a8fb05359de59ceddf5a4ad837ea16241d5
Jonas Beyer <reyeb.sanoj@googlemail.com>
2024-10-19 22:08:13 +0200

    search and execute "sx" fish function

---
+++ dotfiles/.config/fish/functions/sx.fish
@@ -0,0 +1,21 @@
+
+# search and execute, via fzf, default search is ls, default execute is open
+function sx
+    if test (count $argv) -eq 0
+        set cmd "ls"
+    else
+        set cmd $argv
+    end
+    
+    set selection (eval $cmd | fzf)
+    
+    echo "Enter a command to append the fzf selection (default: open):"
+    set user_cmd
+    read user_cmd
+    
+    if test -z "$user_cmd"
+        set user_cmd "open"
+    end
+    
+    eval $user_cmd $selection
+end
