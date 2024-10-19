29ab08bc3d61eef798740e695316c1056f6c573b
Jonas Beyer <reyeb.sanoj@googlemail.com>
2024-10-19 22:37:13 +0200

    sxh - sx version that opens in a hyprland window

---
+++ dotfiles/.config/fish/functions/sxh.fish
@@ -0,0 +1,17 @@
+
+# search and execute, via fzf, default search is ls, default execute is open
+function sx
+    if test (count $argv) -eq 0
+        set cmd "ls"
+    else
+        set cmd $argv
+    end
+    
+    set selection (eval $cmd | fzf)
+    
+    echo "Open with"
+    set user_cmd
+    read user_cmd
+    
+    eval hyprctl dispatch exec $user_cmd $selection
+end
