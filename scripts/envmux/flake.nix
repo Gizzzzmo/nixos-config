b9f8a08528761897ab0b6067722b62373ede7af5
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-04-06 12:12:50 +0200

    add new sourcemux script

--- scripts/envmux/flake.nix
+++ scripts/envmux/flake.nix
@@ -7,18 +7,25 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        my-name = "envmux";
         my-buildInputs = with pkgs; [ coreutils perl ];
-        my-script = (pkgs.writeScriptBin my-name (builtins.readFile ./envmux.sh)).overrideAttrs(old: {
+        envmux-name = "envmux";
+        envmux-script = (pkgs.writeScriptBin envmux-name (builtins.readFile ./envmux.sh)).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+        sourcemux-name = "sourcemux";
+        sourcemux-script = (pkgs.writeScriptBin sourcemux-name (builtins.readFile ./sourcemux.sh)).overrideAttrs(old: {
           buildCommand = "${old.buildCommand}\n patchShebangs $out";
         });
       in rec {
         defaultPackage = packages.envmux;
         packages.envmux = pkgs.symlinkJoin {
-          name = my-name;
-          paths = [ my-script ] ++ my-buildInputs;
+          name = "tmux-utils";
+          paths = [ envmux-script sourcemux-script ] ++ my-buildInputs;
           buildInputs = [ pkgs.makeWrapper ];
-          postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+          postBuild = ''
+            wrapProgram $out/bin/${envmux-name} --prefix PATH : $out/bin
+            wrapProgram $out/bin/${sourcemux-name} --prefix PATH : $out/bin
+          '';
         };
       }
     );   
