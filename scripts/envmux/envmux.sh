9bc64143758b096c2a8592b338bd01d1d09a3013
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-04-06 12:34:08 +0200

    update envmux

--- scripts/envmux/envmux.sh
+++ scripts/envmux/envmux.sh
@@ -2,7 +2,8 @@
 
 session_name=$(basename $(pwd))
 
-tmux -u new-session -d -s $session_name
+# create new session with session_name, if that fails, then there already exists a session with that name, so we just switch to that session
+tmux new-session -d -s $session_name 2> /dev/null || ( tmux switch -t $session_name && exit )
 
 env -0 | perl -0 -ne 'print "$1\n" if /^([^=]+)=/' | while IFS= read -r line; do
     tmux setenv -t $session_name $line "${!line}"
@@ -13,4 +14,9 @@ tmux kill-window -t $session_name:1
 tmux new-window -t $session_name:
 tmux kill-window -t $session_name:2
 
-tmux attach -t $session_name
+# if TMUX variable is not set attach, otherwise switch to the session
+if [ -z "$TMUX" ]; then
+    tmux attach -t $session_name
+else
+    tmux switch -t $session_name
+fi
