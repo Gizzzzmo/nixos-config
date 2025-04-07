7f76a25b9ebba40bd42f6de25bf6c2633fbb16d2
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-04-07 17:09:08 +0200

    add multimux script

--- scripts/envmux/flake.nix
+++ scripts/envmux/flake.nix
@@ -16,15 +16,20 @@
         sourcemux-script = (pkgs.writeScriptBin sourcemux-name (builtins.readFile ./sourcemux.sh)).overrideAttrs(old: {
           buildCommand = "${old.buildCommand}\n patchShebangs $out";
         });
+        multimux-name = "multimux";
+        multimux-script = (pkgs.writeScriptBin multimux-name (builtins.readFile ./multimux.sh)).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
       in rec {
         defaultPackage = packages.envmux;
         packages.envmux = pkgs.symlinkJoin {
           name = "tmux-utils";
-          paths = [ envmux-script sourcemux-script ] ++ my-buildInputs;
+          paths = [ envmux-script sourcemux-script multimux-script ] ++ my-buildInputs;
           buildInputs = [ pkgs.makeWrapper ];
           postBuild = ''
             wrapProgram $out/bin/${envmux-name} --prefix PATH : $out/bin
             wrapProgram $out/bin/${sourcemux-name} --prefix PATH : $out/bin
+            wrapProgram $out/bin/${multimux-name} --prefix PATH : $out/bin
           '';
         };
       }
