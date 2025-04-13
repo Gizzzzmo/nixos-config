213fed5b56b45bd03fd70d780416038cc38ae539
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-04-13 21:38:52 +0200

    bat theme

--- programs/bat.nix
+++ programs/bat.nix
@@ -7,7 +7,7 @@
         owner = "folke";
         repo = "tokyonight.nvim"; # Bat uses sublime syntax for its themes
         rev = "b262293ef481b0d1f7a14c708ea7ca649672e200";
-        sha256 = "b262293ef481b0d1f7a14c708ea7ca649672e200";
+        sha256 = "sha256-pMzk1gRQFA76BCnIEGBRjJ0bQ4YOf3qecaU6Fl/nqLE";
       };
       file = "extras/sublime/tokyonight_night.tmTheme";
     };
