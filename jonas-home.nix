1a8f350da2086805e7c5ba7320edeaafca8a42db
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-05-10 12:07:04 +0200

    improve hyprland: - fix volume adjustment - add and customize hyprlock lockscreen - allow switching keyboard layouts - add nemo file explorer

--- jonas-home.nix
+++ jonas-home.nix
@@ -1,5 +1,5 @@
 {
-  nixpkgs,
+  #000000#000000  nixpkgs,
   config,
   pkgs,
   inputs,
@@ -68,6 +68,7 @@
       else
         with pkgs; [
           # texpresso
+          nemo
           kitty
           alacritty
           pavucontrol
@@ -159,6 +160,87 @@
   programs.fzf = {
     enable = true;
     enableFishIntegration = true;
+  };
+
+  programs.hyprlock = {
+    enable = !standalone;
+
+    settings = {
+      general = {
+        disable_loading_bar = false;
+        grace = 0;
+        hide_cursor = false;
+        no_fade_in = true;
+      };
+
+      auth = {
+        enabled = true;
+      };
+
+      animations = {
+        enabled = false;
+      };
+
+      background = [
+        {
+          path = "screenshot";
+          blur_passes = 3;
+          blur_size = 8;
+        }
+      ];
+
+      input-field = [
+        {
+          size = "300, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = false;
+          fade_on_empty = false;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgba(5555eeff) rgba(222288ff) 45deg";
+          check_color = "rgba(5555eeff) rgba(222288ff) 45deg";
+          fail_color = "rgba(ff6633ee) rgba(ff0066ee) 40deg";
+          outline_thickness = 3;
+          placeholder_text = "Password...";
+          shadow_passes = 0;
+          fail_text = "$PAMFAIL";
+        }
+      ];
+
+      label = [
+        {
+          monitor = "";
+          text = "$TIME"; # ref. https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/#variable-substitution
+          font_size = 90;
+          # font_family = $font
+
+          position = "-30, 0";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          monitor = "";
+          text = ''cmd[update:60000] date +"%A, %d %B %Y"''; # update every 60 seconds
+          font_size = 25;
+          # font_family = $font;
+
+          position = "-30, -150";
+          halign = "right";
+          valign = "top";
+        }
+        {
+          monitor = "";
+          text = "$LAYOUT[en,de]";
+          font_size = 18;
+          onclick = "hyprctl switchxkblayout all next";
+
+          position = "250, -20";
+          halign = "center";
+          valign = "center";
+        }
+      ];
+    };
   };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
