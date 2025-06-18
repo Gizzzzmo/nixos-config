edca11e2824f1f20ffc3f44dcdfcd30e4696ac99
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-06-18 23:41:24 +0200

    nixvim: avante update deprecated settings

--- programs/neovim/avante.nix
+++ programs/neovim/avante.nix
@@ -1,14 +1,18 @@
 {standalone, ...}: {
   enable = true;
   settings = {
-    claude = {
-      endpoint =
-        if standalone
-        then "fixme"
-        else "https://api.anthropic.com";
-      max_tokens = 4096;
-      model = "claude-3-5-sonnet-20240620";
-      temperature = 0;
+    providers = {
+      claude = {
+        endpoint =
+          if standalone
+          then "fixme"
+          else "https://api.anthropic.com";
+        model = "claude-3-5-sonnet-20240620";
+        extra_request_body = {
+          max_tokens = 4096;
+          temperature = 0;
+        };
+      };
     };
 
     hints.enabled = false;
