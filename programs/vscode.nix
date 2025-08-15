a11d16fa31d250c5b42b58cdf2081d32cdb14c6a
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-08-15 21:07:30 +0200

    simplify vscode

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -2,33 +2,15 @@
   enable = true;
 
   profiles.default = {
-    extensions = with pkgs.vscode-extensions;
-      [
-        rust-lang.rust-analyzer
-        eamodio.gitlens
-        #vscodevim.vim
-        jnoortheen.nix-ide
-        llvm-vs-code-extensions.vscode-clangd
-        ms-vscode.cmake-tools
-        enkia.tokyo-night
-      ]
-      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        # binascii.hexlify(base64.b64decode('8QQmTUIxQZo3owpCNh+5IjtnoNNvd0M1FI3cJrFG5Rg=')).decode("utf-8")
-        #
-        # codeium tries to download its own language server binary which is incompatible with nix's non-fsh compliant filesystem, and it seems to be impossible to point the extension to another location for the language server binary
-        #{
-        #  name = "codeium";
-        #  publisher = "Codeium";
-        #  version = "1.17.4";
-        #  sha256 = "bafae9048f2d7143fae122f5dd4400c2da3ee06614d131b4fb7bb79aa4c8869e";
-        #}
-        {
-          name = "cmake-language-support-vscode";
-          publisher = "josetr";
-          version = "0.0.9";
-          sha256 = "2cdb57619eb92e46b5969c5e2a8ccae8b074c9ac408c7b1f56c089f082d7f22a";
-        }
-      ];
+    extensions = with pkgs.vscode-extensions; [
+      # rust-lang.rust-analyzer
+      eamodio.gitlens
+      #vscodevim.vim
+      jnoortheen.nix-ide
+      llvm-vs-code-extensions.vscode-clangd
+      ms-vscode.cmake-tools
+      enkia.tokyo-night
+    ];
     userSettings = {
       "[nix]"."editor.tabSize" = 2;
       "editor.stickyScroll.enabled" = false;
