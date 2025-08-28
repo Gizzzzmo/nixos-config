092bbb6953c375b255b2cf9d13442f90b01e2c23
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-08-28 21:05:16 +0200

    nixvim: reduce lualine content

--- programs/neovim/lualine.nix
+++ programs/neovim/lualine.nix
@@ -6,10 +6,26 @@
     options.section_separators = "";
     options.theme = "iceberg_dark";
     sections = {
+      lualine_a = [
+        {
+          __unkeyed-1 = "git_branch";
+          icon = "";
+        }
+        {
+          __unkeyed-1 = "git_diff";
+          colored = true;
+          always_visible = false;
+        }
+      ];
       lualine_c = [
         {
           __unkeyed-1 = "filename";
           path = 1;
+        }
+      ];
+      lualine_x = [
+        {
+          __unkeyed-1 = "filetype";
         }
       ];
     };
