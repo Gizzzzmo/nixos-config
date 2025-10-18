d383f59c4777832ac67241e3e0bc311d89540da6
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-10-18 12:38:47 +0200

    iwd backend for networkmanager wifi

--- configuration.nix
+++ configuration.nix
@@ -24,8 +24,15 @@
 
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;
+  # networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
+  networking.wireless = {
+    # enable = true;
+    iwd.enable = true;
+  };
   # Easiest to use and most distros use this by default.
 
   nix.settings.experimental-features = [
