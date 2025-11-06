f39e0f55633b4833e585e3c8b1ba8486cdfda25f
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-11-06 10:34:50 +0100

    disable iwd wireless backend

--- configuration.nix
+++ configuration.nix
@@ -23,17 +23,13 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = false; # Enables wireless support via wpa_supplicant.
   networking.networkmanager = {
     enable = true;
-    wifi.backend = "iwd";
+    # wifi.backend = "iwd";
   };
-  networking.wireless = {
-    # enable = true;
-    iwd.enable = true;
-  };
-  # Easiest to use and most distros use this by default.
+  # networking.wireless = {
+  #   iwd.enable = true;
+  # };
 
   nix.settings.experimental-features = [
     "nix-command"
