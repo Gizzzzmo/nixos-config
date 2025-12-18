e634e1a2b76f81a22388f2fa1a1b07cf4a441c7f
Jonas Beyer <reyeb.sanoj@googlemail.com>
2025-12-18 10:33:12 +0100

    fixup neogen

--- programs/neovim/neogen.nix
+++ programs/neovim/neogen.nix
@@ -1,13 +1,17 @@
-{standalone, ...}:
-{
+{standalone, ...}: {
   enable = true;
+
+  settings = {
+    snippet_engine = "luasnip";
+  };
 }
-// (
-  if standalone
-  then {snippetEngine = "luasnip";}
-  else {
-    settings = {
-      snippet_engine = "luasnip";
-    };
-  }
-)
+# // (
+#   if standalone
+#   then {snippetEngine = "luasnip";}
+#   else {
+#     settings = {
+#       snippet_engine = "luasnip";
+#     };
+#   }
+# )
+
