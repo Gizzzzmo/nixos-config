e03f9d8e7c69cebadf34fd2ec13e5685eec99d7e
Jonas Beyer <reyeb.sanoj@googlemail.com>
2026-01-19 15:45:47 +0100

    fish: cleanup init

--- programs/fish.nix
+++ programs/fish.nix
@@ -31,16 +31,29 @@
       bind --mode insert ctrl-f 'accept-autosuggestion'
       bind --mode insert ctrl-p up-or-search
       bind --mode insert ctrl-n down-or-search
-      set -x GPG_TTY (tty)
+      # set -x GPG_TTY (tty)
       set -x MANPAGER "nvim +Man!"
-      if test -z $SSH_AGENT_PID
-        bass eval (ssh-agent -s)
+      if test -z "$SSH_AGENT_PID"
+        # Parse ssh-agent output natively in Fish
+        for line in (ssh-agent -s)
+          # Extract variable assignments like "SSH_AUTH_SOCK=/tmp/...; export SSH_AUTH_SOCK;"
+          if string match -q -r '^(SSH_[^=]+)=([^;]+)' -- $line
+            set -l var (string replace -r '^(SSH_[^=]+)=([^;]+).*' '$1' -- $line)
+            set -l val (string replace -r '^(SSH_[^=]+)=([^;]+).*' '$2' -- $line)
+            # Remove quotes if present
+            set val (string trim -c '\'"' -- $val)
+            set -gx $var $val
+          end
+        end
         ssh-add | true
       end
-      eval (direnv hook fish)
+      # eval (direnv hook fish)
     end
-    if test -f $HOME/.config/api-keys.sh
-      bass source $HOME/.config/api-keys.sh
+    if test -f $HOME/.config/api-keys.fish
+      bass source $HOME/.config/api-keys.fish
+    end
+    status --is-login; and begin
+      envmux
     end
   '';
 
