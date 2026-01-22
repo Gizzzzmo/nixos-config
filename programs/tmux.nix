ccca5f82ac46d03d5edd01a379eb6ad74308177e
Jonas Beyer <reyeb.sanoj@googlemail.com>
2026-01-22 12:20:11 +0100

    modularize tmux configs for servers

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -13,7 +13,10 @@
     fingers
   ];
   extraConfig =
-    ''
+    # Basic config extracted to basic-tmux.conf for portability
+    builtins.readFile ./basic-tmux.conf
+    + ''
+      # NixOS-specific additions
       run-shell ${
         (fetchGit {
           url = "https://github.com/artemave/tmux_super_fingers";
@@ -22,107 +25,8 @@
         .outPath
       }/tmux_super_fingers.tmux
 
-      set -g prefix C-b
-      set -s escape-time 0
-      set -g mouse on
-      set -g status-style bg=colour8,fg=colour15
-      set -g mode-style fg=colour236,bg=colour15
-      set -g detach-on-destroy off
-      set-window-option -g window-status-current-style bg=colour15,fg=colour0
-
       set -g update-environment 'DISPLAY ALACRITTY_WINDOW_ID ALACRITTY_LOG ALACRITTY_SOCKET DISPLAY WINDOWID SSH_ASKPASS SSH_CONNECTION XAUTHORITY KITTY_WINDOW_ID KITTY_PID'
-
       set -g default-shell /home/jonas/.nix-profile/bin/fish
-      set -g status-left-length 30
-      set -gq allow-passthrough on
-      set -g visual-activity off
-
-      bind -r h select-pane -L
-      bind -r j select-pane -D
-      bind -r k select-pane -U
-      bind -r l select-pane -R
-
-      bind -r % split-window -h
-      bind -r '"' split-window -v
-
-      bind -n M-w last-window
-      bind -n M-= select-window -n
-      bind -n M-- select-window -p
-      bind -n M-0 select-window -t 0
-      bind -n M-1 select-window -t 1
-      bind -n M-2 select-window -t 2
-      bind -n M-3 select-window -t 3
-      bind -n M-4 select-window -t 4
-      bind -n M-5 select-window -t 5
-      bind -n M-6 select-window -t 6
-      bind -n M-7 select-window -t 7
-      bind -n M-8 select-window -t 8
-      bind -n M-9 select-window -t 9
-
-      bind-key -Tcopy-mode-vi 'v' send -X begin-selection
-      bind-key -Tcopy-mode-vi 'y' send -X copy-selection
-      # disable "release mouse drag to copy and exit copy-mode", ref: https://github.com/tmux/tmux/issues/140
-      unbind-key -T copy-mode-vi MouseDragEnd1Pane
-
-      # since MouseDragEnd1Pane neither exit copy-mode nor clear selection now,
-      # let single click do selection clearing for us.
-      bind-key -T copy-mode-vi MouseDown1Pane select-pane\; send-keys -X clear-selection
-      # this line changes the default binding of MouseDrag1Pane, the only difference
-      # is that we use `copy-mode -eM` instead of `copy-mode -M`, so that WheelDownPane
-      # can trigger copy-mode to exit when copy-mode is entered by MouseDrag1Pane
-      bind -n MouseDrag1Pane if -Ft= '#{mouse_any_flag}' 'if -Ft= \"#{pane_in_mode}\" \"copy-mode -eM\" \"send-keys -M\"' 'copy-mode -eM'
-
-      bind -n 'M-[' copy-mode
-
-      bind -r r source ~/.config/tmux/tmux.conf
-
-      vim_pattern='(\S+/)?g?\.?(view|l?n?vim?x?|fzf)(diff)?(-wrapped)?'
-      is_vim="ps -o state= -o comm= -t '#{pane_tty}'  | grep -iqE '^[^TXZ ]+ +''${vim_pattern}$'"
-      bind-key -n 'M-h' if-shell "$is_vim" 'send-keys M-h'  'select-pane -L'
-      bind-key -n 'M-j' if-shell "$is_vim" 'send-keys M-j'  'select-pane -D'
-      bind-key -n 'M-k' if-shell "$is_vim" 'send-keys M-k'  'select-pane -U'
-      bind-key -n 'M-l' if-shell "$is_vim" 'send-keys M-l'  'select-pane -R'
-      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
-      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
-      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
-          "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
-
-      bind-key -T copy-mode-vi 'M-h' select-pane -L
-      bind-key -T copy-mode-vi 'M-j' select-pane -D
-      bind-key -T copy-mode-vi 'M-k' select-pane -U
-      bind-key -T copy-mode-vi 'M-l' select-pane -R
-      bind-key -T copy-mode-vi 'C-\' select-pane -l
-
-      # Key binding to toggle key bindings on and off
-      color_window_off_status_bg="colour237"
-      color_window_off_status_current_bg="colour241"
-      color_window_off_indicator="colour160"
-      color_status_text="colour250"
-      color_dark="colour234"
-      color_light="colour254"
-      separator_powerline_right=""
-
-      bind -T root F12  \
-        set prefix None \;\
-        set key-table off \;\
-        set status-style "fg=$color_status_text,bg=$color_window_off_status_bg" \;\
-        set window-status-current-format "#[fg=$color_window_off_status_bg,bg=$color_window_off_status_current_bg]$separator_powerline_right#[default]#I:#W*#[fg=$color_window_off_status_current_bg,bg=$color_window_off_status_bg]$separator_powerline_right#[default]" \;\
-        set window-status-current-style "fg=$color_dark,bg=$color_window_off_status_current_bg" \;\
-        if -F '#{pane_in_mode}' 'send-keys -X cancel' \;\
-        refresh-client -S \;\
-
-      bind -T off F12 \
-        set -u prefix \;\
-        set -u key-table \;\
-        set -u status-style \;\
-        set -u window-status-current-style \;\
-        set -u window-status-current-format \;\
-        refresh-client -S
-
-      wg_is_keys_off="#[fg=$color_light,bg=$color_window_off_indicator]#([ $(tmux show-option -qv key-table) = 'off' ] && echo 'OFF')#[default]"
-
-      set -g status-right "$wg_is_keys_off #{sysstat_cpu} | #{sysstat_mem} | #{sysstat_loadavg} | $wg_user_host"
     ''
     + (
       if standalone
