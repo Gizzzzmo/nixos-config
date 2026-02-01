da74c9f3bac230708070123bd3ad33a019b01ac3
Jonas Beyer <reyeb.sanoj@googlemail.com>
2026-02-01 14:08:57 +0100

    qutebrowser: allow gmail, and clipboard

--- programs/qutebrowser.nix
+++ programs/qutebrowser.nix
@@ -2,6 +2,11 @@
   enable = !standalone;
   settings = {
     content.blocking.method = "both";
+    content.javascript.clipboard = "access";
     colors.webpage.darkmode.enabled = true;
   };
+  extraConfig = ''
+    config.set("content.register_protocol_handler", True, "https://mail.google.com?extsrc=mailto&url=%s")
+    config.load_autoconfig()
+  '';
 }
